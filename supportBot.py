533a97eff67a980f2cd4f631ec1fc5ca2b06e392
SCRATCHER 5-23 <86705803+5-23@users.noreply.github.com>
2022-01-23 23:32:01 +0900

    Update supportBot.py

--- supportBot.py
+++ supportBot.py
@@ -26,23 +26,27 @@ def random_color():
 @client.event
 async def on_ready():
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nstart")
-    
+
 @client.event
 async def send(message = None , embed = None):
     await client.get_channel(934793904631459870).send(message , embed=embed)
 
-@client.slash_command(description="개발자만 사용가능")
+@client.slash_command(description="개발자만 사용 가능.")
 async def 답변(inter : Interaction , id , message):
     member = utils.get(client.get_all_members(),id = int(str(id).replace(" ","")))
-    await member.send(embed = Embed(title = f"**{member.name}**님 개발자에게서 답변이 왔어요" , description=f"```\n{message}\n```" , color = random_color() ))
-    await inter.response.send_message(f"답변완료\n```\n{message}\n```")
+    await member.send(embed = Embed(title = f"{member.name}님, 개발자에게서 답변이 왔어요." , description=f"
+```\n{message}\n```
+" , color = random_color() ))
+    await inter.response.send_message(f"답변 완료.\n
+```\n{message}\n```
+")
 
 @client.event
 async def on_message(message):
     try:
         if str(message.channel.type) == "private":
             if message.author.bot == False:
-                embed = Embed(title = f"문의를 하실건가요?",description = f"내용 : {message.content}",timestamp=message.created_at , color = random_color())
+                embed = Embed(title = f"문의를 하실 건가요?",description = f"내용: {message.content}",timestamp=message.created_at , color = random_color())
                 try: 
                     img = str(message.attachments[0])
                     embed.set_image(url = img)
@@ -54,9 +58,9 @@ async def on_message(message):
         pass
 
 class yes(ui.View):
-    @ui.button(label="네" , style=ButtonStyle.green , emoji="<:vv:905014667632594994>")
+    @ui.button(label="네" , style=ButtonStyle.green , emoji=":vv:")
     async def yes(self , button : Button , inter : Integration):
-        embed = Embed(title = "문의가 왔어요!" , description=f"{inter.message.embeds[0].description.replace('내용 : ' , '')} \n\nid : {inter.user.id}\nname:{inter.user}" , color = random_color())
+        embed = Embed(title = "문의가 왔습니다." , description=f"{inter.message.embeds[0].description.replace('내용 : ' , '')} \n\nid : {inter.user.id}\nname:{inter.user}" , color = random_color())
         try:
             if "http" in str(inter.message.embeds[0].url):
                 embed.set_image(url = str(inter.message.embeds[0].url))
@@ -64,10 +68,10 @@ class yes(ui.View):
             pass
         print(inter.message.embeds[0].description.replace("내용 : " , ""))
         await send(embed=embed )
-        await inter.response.send_message(embed = Embed(title = "문의가 완료됐습니다" , color = random_color()))
-    @ui.button(label="아니요" , style=ButtonStyle.red , emoji="<:xx:905014703577772063>")
+        await inter.response.send_message(embed = Embed(title = "문의가 완료 되었습니다." , color = random_color()))
+    @ui.button(label="아니요." , style=ButtonStyle.red , emoji=":xx:")
     async def no(self , button : Button , inter : Integration):
         await inter.message.delete()
-        
+
 token = os.environ['BOT_TOKEN']
 client.run(token)
